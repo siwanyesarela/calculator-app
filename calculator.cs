e932edc8c8f8d99ee37f65a153c199d1f4841afe
siwanyesarela <siwanyesarela@students.amikom.ac.id>
2023-03-16 14:40:22 +0700

    add file calculator

---
+++ calculator.cs
@@ -0,0 +1,17 @@
+class calculator
+{
+	static void main (string[] args)
+	{
+		
+	}
+	
+	static int penambahan(int a,int b)
+	{
+		return a + b;
+	}
+	static int pengurangan(int a,int b)
+	{
+		return a-b;
+	}
+
+}
